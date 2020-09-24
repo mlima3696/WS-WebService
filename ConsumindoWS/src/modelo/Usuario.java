ad22effb1043309de53fa4f69b08c55e93d3f73b
mlima3696 <mjuniorlima3@gmail.com>
2020-09-24 18:34:30 -0300

    Consumindo no Java Parte-1.1

---
+++ ConsumindoWS/src/modelo/Usuario.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author marcelosiedler
+ */
+public class Usuario {
+    
+    private String login;
+    private int codigo;
+    private String desc_produto;
+    private Double preco;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    
+    
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDesc_produto() {
+        return desc_produto;
+    }
+
+    public void setDesc_produto(String desc_produto) {
+        this.desc_produto = desc_produto;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    
+
+    
+}
